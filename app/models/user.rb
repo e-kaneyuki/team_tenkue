8f9eecd5e1dafc889a1e39c3d806a9dd9f003243
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-12-14 18:17:38 +0900

    fix: post-index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< HEAD
 end
+=======
+  has_many :posts
+end
+>>>>>>> 1941528... 掲示板一覧表示機能
