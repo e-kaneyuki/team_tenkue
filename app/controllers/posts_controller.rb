274c28b462583df167f82f3ebb2c8aa59a791d3a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-16 21:47:45 +0900

    showアクションの追加とビューファイルの移動

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user).order('created_at DESC')
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
