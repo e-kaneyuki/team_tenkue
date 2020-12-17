b0db094a4b72dc7ee055ed7ab068869482f972c4
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-17 14:47:44 +0900

    掲示板詳細表示機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destoy]
+  before_action :move_to_index, only: [:edit, :update, :destoy]
+  
   def index
     @posts = Post.all.includes(:user).order(id: "DESC")
   end
@@ -6,4 +9,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @post.user_id
+      redirect_to root_path
+    end
+  end
+
 end
