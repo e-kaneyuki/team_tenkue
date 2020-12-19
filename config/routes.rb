9cfa0943240d659a39ab42ff3c12079a02480867
Kouhei <hiyotihakou@gmail.com>
2020-12-20 04:54:24 +0900

    byebug の削除 & route のposts#new を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  root to: "posts#new"
   resources :posts
   devise_for :users, controllers: {
     registrations: 'users/registrations',
