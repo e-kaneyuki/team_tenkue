599d47957d7b10493b71cef07358954315070689
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-12-19 12:51:19 +0900

    controllerとveiwsの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,39 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user).order(id: "DESC")
   end
+
+  def show
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    if current_user.id != @post.user_id
+      redirect_to posts_url
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if current_user.id == @post.user_id && @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if current_user.id == @post.user_id
+      @post.destroy
+      flash[:danger] = "「#{@post.content}」を削除しました。"
+      redirect_to posts_url
+    end
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
