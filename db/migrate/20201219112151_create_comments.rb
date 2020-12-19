c76d3c581943bcfab2b52e524a74493c0884d19b
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-19 20:50:32 +0900

    commentsテーブルの作成

--- db/migrate/20201219112151_create_comments.rb
+++ db/migrate/20201219112151_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.text :comment
+      t.references :user, type: :bigint, foreign_key: true
+      t.references :post, type: :bigint, foreign_key: true
       t.timestamps
     end
   end
