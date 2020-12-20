1aafd871d2e6b7658acdb6db4d31aaeb10407653
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-12-20 12:05:40 +0900

    route.rbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:index, :create, :edit, :update, :destroy]
+  end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
