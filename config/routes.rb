812eaab2b89978071c227f29d58a473d066ec516
e-kaneyuki <occultchikin@gmail.com>
2020-12-24 07:35:53 +0900

    routes.rb のコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-    # delete 'logout'  => 'sessions#destroy'
     delete 'users/guest_logout', to: 'users/sessions#destroy'
   end
 end
