9dd64c641da95de7b7c2f36afb76206cfd336db6
e-kaneyuki <occultchikin@gmail.com>
2020-12-30 22:43:06 +0900

    作業ブランチの切り替え忘れに伴うコード修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
-class CommentsController < ApplicationController
 
+class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -10,8 +10,6 @@ class CommentsController < ApplicationController
       redirect_to post_path(@post.id)
     end
   end
-  def edit
-  end
 
   def destroy
     # <byebug>
@@ -20,23 +18,9 @@ class CommentsController < ApplicationController
     redirect_to post_path(params[:post_id])
   end
 
-
-  def edit
-    @comment = current_user.comments.find(params[:id])
-  end
-
-  def destroy
-    @comment = Comment.find(comment_params)
-    if @comment.destroy
-      redirect_to post_path(@post), notice: 'コメントを削除しました'
-    else
-      flash.now[:alert] = 'コメント削除に失敗しました'
-      render post_path(@post)
-    end
-  end
   private
 
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, :comment_params, post_id: params[:post_id, :id])
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
