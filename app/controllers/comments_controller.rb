230e3d3e738b196e9793427071f6197f4b7ba692
e-kaneyuki <occultchikin@gmail.com>
2021-01-01 13:02:20 +0900

    edit機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,24 @@ class CommentsController < ApplicationController
       redirect_to post_path(@post.id)
     end
   end
+  # byebug
+  def edit
+    # byebug
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    render "posts/show"
+    # @comment = Comment.find(params[:id])
+    # redirect_to post_path(params[:post_id])
+    # ↑ここの設定次第です ここの設定でどこの画面にとんで編集をするのかを設定したい。
+    # ↑それが決まれば次のメソッドでupdateができるはず。
+  end
 
+  def update
+    # byebug
+    # @comment = Comment.find(params[:id])
+    @comment.update(params[:comment])
+    redirect_to post_path(@post.id)
+  end
   def destroy
     # <byebug>
     @comment = Comment.find(params[:id])
@@ -23,4 +40,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
   end
+
 end
