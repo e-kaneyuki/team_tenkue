72a66a71e2a6e0bedee0c8cbbc6d69caf64b94d0
e-kaneyuki <occultchikin@gmail.com>
2021-01-03 01:44:27 +0900

    文字数制限(140文字)を超えた場合にエラーメッセージを表示する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,21 +16,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     render "posts/show"
-    # @comment = Comment.find(params[:id])
-    # redirect_to post_path(params[:post_id])
-    # ↑ここの設定次第です ここの設定でどこの画面にとんで編集をするのかを設定したい。
-    # ↑それが決まれば次のメソッドでupdateができるはず。
   end
 
   def update
     @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       redirect_to post_path(params[:post_id])
-      #↑これredirect_to request.referera の表現を少し変えてしっかり変更が反映されるようにしましょう！
-      #データベースには変更の受け渡しはできている
     else
-      flash[:alert] = "更新できませんでした"
-      render "posts/show"
+      flash[:alert] = 'コメントを(140文字以内で)入力してください。'
+      render 'posts/show'
     end
   end
   def destroy
