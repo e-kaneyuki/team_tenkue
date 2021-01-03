427d35b6e35689ae3ace21584ca17d7e3a523c99
e-kaneyuki <occultchikin@gmail.com>
2021-01-03 17:31:57 +0900

    投稿者のみが削除できるようルーティング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
     end
   end
   def destroy
-    @comment = Comment.find(params[:id])
+    @comment = current_user.comments.find(params[:id])
     @comment.destroy
     flash[:notice] = "コメントを削除しました"
     redirect_to post_path(params[:post_id])
